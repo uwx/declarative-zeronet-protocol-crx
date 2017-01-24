ba708d7c513b9359f4588a91e23c5f47817577df
mkg20001 <mkg20001@gmail.com>
2017-01-24 19:41:01 +0100

    Update the settings page

--- js/settings.js
+++ js/settings.js
@@ -1,70 +1,73 @@
-var Settings = (function () {
-
-  function Settings() {
-    // constructor
-    this.DEFAULT_IP = "127.0.0.1";
-    this.DEFAULT_PORT = "43110";
-
-    this.ipTextField = document.getElementById("js-ip_tf");
-    this.ipTextField.placeholder = this.DEFAULT_IP;
-
-    this.portTextField = document.getElementById("js-port_tf");
-    this.portTextField.placeholder = this.DEFAULT_PORT;
-
-    // map binds
-    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
-    this.onClearButtonClick = this.onClearButtonClick.bind(this);
-    this.updateCurrentValues = this.updateCurrentValues.bind(this);
-
-    this.updateCurrentValues();
-
-    // to clear local storage - useful for dev
-    //chrome.storage.local.clear();
-
-    var saveButton = document.getElementById("js-save_btn");
-    saveButton.addEventListener("click", this.onSaveButtonClick);
-
-    var clearButton = document.getElementById("js-clear_btn");
-    clearButton.addEventListener("click", this.onClearButtonClick);
+const defaults={
+  ip:"127.0.0.1",
+  port:"43110"
+}
+const validate={
+  ip:(ip) => {
+    return ip.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/)
+  },
+  port:(obj) => {
+    return !jQuery.isArray( obj ) && (obj - parseFloat( obj ) + 1) >= 0;
   }
+}
+const fields=["ip","port"]
 
-  Settings.prototype.onSaveButtonClick = function () {
-    var zeroHostData = this.ipTextField.value.trim() === "" ? this.DEFAULT_IP : this.ipTextField.value;
-    zeroHostData += ":" + (this.portTextField.value.trim() === "" ? this.DEFAULT_PORT : this.portTextField.value);
+var err=false
 
+function showError(e) {
+  err=e
+  $("#err").html(e)
+  $("#error").show()
+}
+
+function hideError() {
+  err=false
+  $("#error").hide()
+}
+
+function grab() {
+  var res={}
+  fields.map((f) => {
+    var v=$("#"+f+"_field").val().trim()||defaults[f]
+    if (v.indexOf(":")!=-1) return showError("Invalid value for field "+(f.substr(0,1).toUpperCase() + f.substr(1))+" - Please seperate the Port from the IP")
+    if (!validate[f](v)) return showError("Invalid value for field "+(f.substr(0,1).toUpperCase() + f.substr(1)))
+    res[f]=v
+  })
+  return res;
+}
+
+function setField(f,v) {
+  $("#"+f+"_field").val(v)
+}
+
+const actions={
+  save:() => {
+    hideError()
+    var r=grab()
+    if (err) return;
     chrome.storage.local.set({
-      "zeroHostData": zeroHostData
+      "zeroHostData": r.ip+":"+r.port
     }, function () {
       window.close()
-      alert("Saved!");
     });
-
-  };
-
-  Settings.prototype.onClearButtonClick = function () {
-    this.ipTextField.value = "";
-    this.portTextField.value = "";
-  };
-
-  Settings.prototype.updateCurrentValues = function () {
-    var thisObject = this;
-    chrome.storage.local.get(function (item) {
-      console.log(item.zeroHostData);
-      if (item && item.zeroHostData !== undefined) {
-        var data = item.zeroHostData.split(':');
-        thisObject.ipTextField.value = data[0];
-        thisObject.portTextField.value = data[1];
-      }
-
-    });
-  };
-
-  return Settings;
-})();
-
-window.addEventListener("load", onLoadComplete);
-
-function onLoadComplete() {
-  //alert("done");
-  new Settings();
+  },
+  clear:() => {
+    fields.map((f) => {setField(f,"")})
+  }
 }
+
+function load() {
+  chrome.storage.local.get(function (item) {
+    if (item && item.zeroHostData !== undefined) {
+      var data = item.zeroHostData.split(':');
+      setField("ip",data[0])
+      setField("port",data[1])
+    }
+  });
+
+  Object.keys(actions).map((a) => {
+    $("#btn_"+a).on("click",actions[a].bind(this))
+  })
+}
+
+window.addEventListener("load", load);
